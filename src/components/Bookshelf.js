55f6fe33c5c909a46e720049b5e94fa8935abce8
= <ashleydelvento@gmail.com>
2020-04-12 00:38:15 -0400

    Optimized for scalability and search.

--- src/components/Bookshelf.js
+++ src/components/Bookshelf.js
@@ -1,28 +1,100 @@
 import React, { Component } from 'react'
-import peterRabbitCover from '../assets/peterRabbitCover.jpg';
-import tortHareCover from '../assets/TortoiseHareCover.jpg';
-import littleRedHenCover from '../assets/littleRedHenCover.jpg';
+import logo from '../assets/logo.png';
+import search from '../assets/search.png';
+import StoryText from './StoryText.js';
 
 class Bookshelf extends Component {
-
+    constructor(props) {
+        super(props);
+        this.state = {
+            storyList: this.initialBookStack(),
+            filterStoryList: this.initialBookStack()
+        }
+    }
+/**
+ * Pushes the path of the page we are going to to said page
+ * @param {*} path 
+ */
     nextPath(path) {
         this.props.history.push(path);
     }
-
+/**
+ * At Bookshelf mount - deletes buttons from past storybook if they exist and loads the default bookshelf
+ */
     componentDidMount() {
+        this.deleteButtons();
+    }
+/**
+ * Deletes buttons from last story clicked on
+ */
+    deleteButtons() {
         var wordBtns = document.getElementsByClassName('btn');
         while (wordBtns[0]) {
             wordBtns[0].parentNode.removeChild(wordBtns[0]);
         }
     }
-    
+/**
+ * Sets up the initial array of default books for the bookshelf
+ */
+    initialBookStack() {
+        let loadedBooks = []
+        for (var i = 0; i < Object.keys(StoryText).length; i++) {
+            loadedBooks.push(Object.keys(StoryText)[i])
+        }
+        return loadedBooks;
+    }
+/**
+ * filters through which books are displayed based on the text typed into the search bar and each books SearchTerms
+ * @param {*} input 
+ */
+    filter(input) {
+        let storyObject;
+        let searchInput;
+        let outputArray = [];
+        if (input.target.value !== "") {
+            for (var i = 0; i < Object.keys(StoryText).length; i++) {
+                storyObject = Object.keys(StoryText)[i]
+                for (var j = 0; j < StoryText[storyObject].SearchTerms.length; j++) {
+                    searchInput = input.target.value.toLowerCase();
+                    
+                    if (StoryText[storyObject].SearchTerms[j].includes(searchInput) && !(outputArray.some(output => output == storyObject))) {
+                        outputArray = outputArray.concat(StoryText[storyObject].Title)
+                    }
+                }
+            }
+        } else {
+            outputArray = outputArray.concat(this.initialBookStack());
+        }
+        if (outputArray != []) {
+            this.setState({
+                filterStoryList: outputArray
+            })
+        } else {
+            //Figure out a way to display 'Sorry we dont seem to have that. Please try another keyword or title.'
+        }
+    }
+
     render() {
+        let bookArray = this.state.filterStoryList
         return (
-            <div class='bookshelf'>
-                <div>
-                    <img class='bookCover' src={peterRabbitCover} onClick={() => this.nextPath('/PeterRabbit')} />
-                    <img class='bookCover' src={tortHareCover} onClick={() => this.nextPath('Tortoise_and_the_Hare')} />
-                    <img class='bookCover' src={littleRedHenCover} onClick={() => this.nextPath('Little_Red_Hen')} />
+            <div >
+                <ul className="Mainheader" >
+                    <img src={logo} className="logo"></img>
+                    <h2>Words to Stories </h2>
+                    <h4>ESL and Vocab Learning made Easy!</h4>
+                    <img src={search} style={{ height: '20px' }}></img>
+                    <input type="text" className="input" onChange={this.filter.bind(this)} placeholder="Search..." />
+                </ul>
+                <div className='bookshelf'>
+                    {
+                        /**
+                         * Creates the elements to display the book covers on screen for each book
+                         */
+                        bookArray.map((value, index) => {
+                            let path = '/' + value;
+                            return <img key={index} className='bookCover' src={StoryText[value].Cover} onClick={() => this.nextPath('/' + value)} />
+                        })
+                    }
                 </div>
             </div>
         );
--- src/components/StoryPage.js
+++ src/components/StoryPage.js
@@ -10,32 +10,45 @@ class StoryPage extends React.Component {
             story: StoryText[this.props.location.pathname.substr(1)]
         }
     }
-
+/**
+ * Splits a string into an array of words using the space character
+ * @param {*} storyString 
+ */
     storyToArray(storyString) {
         return storyString.split(" ");
     }
-
+/**
+ * Checks whether a word is the new paragraph signal or not
+ * @param {*} word 
+ */
     isParagraph(word) {
         if (word === "/n") { return true; }
         else { return false; }
     }
-
+/**
+ * Takes in a word and fetches its data from the dictionary api, then returns it
+ * @param {*} word 
+ */
     async getDef(word) {
         let url = `https://dictionaryapi.com/api/v3/references/learners/json/${word}?key=${key}`
         let response = await fetch(url);
         return await response.json();
     }
-
+/**
+ * Takes in word from button, gets data and then shows data for word name and definition to user
+ * @param {*} word 
+ */
     async printWordDef(word) {
         let data = await this.getDef(word);
         let wordName;
         let firstDefinition;
-        if ((data[0].meta != undefined) && (data[0].shortdef.length != 0)) {
+        if (data[0].meta != undefined && data[0].shortdef.length != 0) {
             wordName = data[0].meta.id;
             firstDefinition = data[0].shortdef;
-        } else {
+        }
+        else {
             wordName = word;
-            firstDefinition = "This word is not found in the dictionary, this could be because it is a name or a word that was created by the author, or it could be because "
+            firstDefinition = "This word is not found in the dictionary, this could be because it is a name or a word that was created by the author, or it is the past tense of another word."
         }
         if (wordName.includes(`:` || `1` || `"` || `'` || `,`)) {
             wordName = wordName.replace(`:`, "");
@@ -44,25 +57,27 @@ class StoryPage extends React.Component {
             wordName = wordName.replace(`"`, "");
             wordName = wordName.replace(`,`, "");
         }
-        console.log(data[0]);
         document.getElementById("wordName").innerHTML = wordName;
         document.getElementById("def").innerHTML = "Common Definition: " + firstDefinition;
     }
 
     render() {
-        let wordArray = this.storyToArray(this.state.story);
+        let wordArray = this.storyToArray(this.state.story.Text);
         return (
             <div>
                 <ul className="header">
                     <li><NavLink to="/">Bookshelf</NavLink></li>
                 </ul>
                 <div className="rightPane">
-                    <h2 id="wordName" className="rightPaneText"> Click on a word you dont know and the definition will appear here. <br/> Haga clic en una palabra que no sabe y la definición aparecerá aquí </h2>
+                    <h2 id="wordName" className="rightPaneText"> Click on a word you dont know and the definition will appear here. <br /> Haz clic en palabra que no sapas y la definición aparecerá aquí </h2>
                     <h4 id="def" className="rightPaneText"></h4>
                 </div>
                 <div className="mainPane" id="mainPane">
                     <div>
                         {
+                            /**
+                             * On render - Creates a button for every word in the story string and then makes paragraph changes when the word is a '/n'
+                             */
                             wordArray.map((value, index) => {
                                 if (this.isParagraph(value) === false) {
                                     return <input key={index} className="btn" type="button" value={value} onClick={() => this.printWordDef(value)}></input>
